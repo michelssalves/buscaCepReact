376d81f1d18d2def89cc688b7d5dd83b74b9ac43
Michel <michelssalves@gmail.com>
2023-06-20 17:49:34 -0300

    2023-06-20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
+import axios from "axios";
 
 function App() {
+  const[input, setInput] = useState('');
+  const[cep, setCep] = useState({})
+
+  async function handleSerach(){
+    
+    if(input === ''){
+      alert('Preencha algum CEP')
+      return
+    }
+
+    try{
+
+      const response = await axios.get(`https://viacep.com.br/ws/${input}/json`);
+ 
+      setCep(response.data);
+      
+    }catch{
+
+      alert("Esse cep não existe!")
+      setInput("")
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div  className="containerInput">
+          <input onChange={(e)=> setInput(e.target.value)}value={input} type="text" placeholder="Digite seu cep..."/>
+          <button onClick={handleSerach} className="buttonSearch">
+          <FiSearch size={25} color='#FFF'/>
+          </button>
+      </div>
+      {Object.keys(cep).length > 0 && (
+
+        <main className='main'>
+          <h2>Cep: {cep.cep}</h2>
+          <span>Estado:{cep.uf}</span>
+          <span>Cidade:{cep.localidade}</span>
+          <span>Bairro:{cep.bairro}</span>
+          <span>Rua:{cep.logradouro}</span>
+          <span>Complemento:{cep.complemento}</span>
+        </main>
+
+      )}
+
     </div>
   );
 }
